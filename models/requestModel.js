fd5abd194086e660877860a210f2d6b1a1ecfcc2
athul <athulsabu75@gmail.com>
2024-03-29 14:42:00 +0530

    updated schema

--- models/requestModel.js
+++ models/requestModel.js
@@ -3,7 +3,27 @@ const mongoose = require('mongoose');
 
 // Define the schema for publisher request
 const requestSchema = new mongoose.Schema({
-    name: {
+    fullName: {
+        type: String,
+        required: true
+    },
+    organizationName: {
+        type: String,
+        required: true
+    },
+    newspaperName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    mobileNumber: {
         type: String,
         required: true
     },
@@ -11,11 +31,59 @@ const requestSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    newspaper: {
+    state: {
+        type: String,
+        required: true
+    },
+    district: {
+        type: String,
+        required: true
+    },
+    buildingName: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    advertisementSlots: {
+        type: String,
+        required: true
+    },
+    fileFormat: {
+        type: String,
+        required: true
+    },
+    paymentmethods: {
+        type: String,
+        required: true
+    },
+    customerService: {
         type: String,
         required: true
     },
     language: {
+        type: String,
+        required: true
+    },
+    bookingDeadline: {
+        type: String,
+        required: true
+    },
+    cancellationRefundPolicy: {
+        type: String,
+        required: true
+    },
+    contentGuidelines: {
+        type: String,
+        required: true
+    },
+    advertisementSubmissionGuidelines: {
+        type: String,
+        required: true
+    },
+    cancellationDeadline: {
         type: String,
         required: true
     },
