c52c98689c4b58477ad79e42cde8068f02c4a8a8
athul <athulsabu75@gmail.com>
2024-04-03 21:59:16 +0530

    updated styles and updated publisher login

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const {
     addPublisher,
     viewPublishers,
+    viewRequest,
+    viewPublisherDetails,
     renderDashboard,
     renderAddPublisher,
+    renderNewspaperSlots,
     deletePublisher,
-    deleteRequest,
-    viewRequest
+    deleteRequest
     } = require("../controllers/adminController");
 
 
@@ -17,12 +19,16 @@ router.route('/add-publisher').get(renderAddPublisher);
 
 router.route('/view-publishers').get(viewPublishers);
 
+router.route('/add-newspaperSlots-details').get(renderNewspaperSlots);
+
 router.route('/view-requests').get(viewRequest);
+
+router.route('/view-request-details/:id').get(viewPublisherDetails);
 
 router.route('/add-publisher').post(addPublisher);
 
-router.route('/delete-request/:id').post(deleteRequest);
-
 router.route('/delete-publisher/:id').post(deletePublisher);
+
+router.route('/delete-request/:id').post(deleteRequest);
 
 module.exports = router;
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -7,7 +7,9 @@ const {
       logoutUser, 
       renderDashboard,  
       verifyOtp,
-      registerUserWithOTP } = require("../controllers/userController");
+      registerUserWithOTP,
+      renderBookSlot,
+      renderBookSlotByDate } = require("../controllers/userController");
 
 
 const router = express.Router();
@@ -36,7 +38,19 @@ app.use(express.static("public"));
 
 router.route("/login").get((req,res) => {
     try{
-      res.render('login.ejs')
+      res.render("login");
+    }
+    catch(err){
+      res.send(err);
+      }
+  });
+
+  router.route("/viewSlot/:layoutName").get((req,res) => {
+    console.log('Hii ftom route');
+    try{
+      const layoutName = req.params.layoutName; // Retrieve layoutName from URL parameter
+      res.render(layoutName);
+      console.log('Hii ftom route');
     }
     catch(err){
       res.send(err);
@@ -73,8 +87,12 @@ router.route("/resetPassword").get((req, res) => {
 
 router.route("/verifyOtp").get((req, res) =>{
     const userEmail = req.query.email;
-    res.render('verify-otp', { email: userEmail });
+    res.render('user/verify-otp', { email: userEmail });
 });
+
+router.route("/viewSlot").get(renderBookSlot);
+
+router.route("/viewSlot").post(renderBookSlotByDate);
 
 router.route("/logout").post(logoutUser);
 
